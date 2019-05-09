4fae4746d0494b345dc0be628e4ade0995234d2d
Daniel Peele <daniel.peele@mail.com>
2019-05-09 18:56:29 -0400

    Added user entity to database Created AuthRepo Interface Implmented Login method connected with database

---
+++ CrmApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using CrmApp.API.Models;
+
+namespace CrmApp.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
--- CrmApp.API/Migrations/20190509220859_AddedUserEntity.Designer.cs
+++ CrmApp.API/Migrations/20190509220859_AddedUserEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CrmApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CrmApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190509220859_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
