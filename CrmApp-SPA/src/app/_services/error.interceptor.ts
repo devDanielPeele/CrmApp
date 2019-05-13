d819b46b05c55bdba892814b7bffeb059c199004
Daniel Peele <daniel.peele@mail.com>
2019-05-13 14:32:07 -0400

    Add third party libraries : -jwt token -alertify -bootswatch
    Main SPA theme changed for bootswatch mint

--- CrmApp-SPA/src/app/_services/error.interceptor.ts
+++ CrmApp-SPA/src/app/_services/error.interceptor.ts
@@ -17,16 +17,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                         console.error(applicationError);
                         return throwError(applicationError);
                     }
-                    const serverError = error.error;
+                    const serverError = error.error.errors;
                     let modalStateErrors = '';
                     if (serverError && typeof serverError === 'object') {
-                        for (const key in serverError.errors) {
-                            if (serverError.errors[key]) {
-                                modalStateErrors += serverError.errors[key] + '\n';
+                        for (const key in serverError) {
+                            if (serverError[key]) {
+                                modalStateErrors += serverError[key] + '\n';
                             }
                         }
                     }
-                    return throwError(modalStateErrors || serverError.errors || 'Server Error');
+                    return throwError(modalStateErrors || serverError || 'Server Error');
                 }
             })
         );
