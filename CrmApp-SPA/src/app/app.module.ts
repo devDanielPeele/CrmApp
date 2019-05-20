ed332881497df1aaf210bcf1b820d6a54d10ca18
devDanielPeele <daniel.peele@mail.com>
2019-05-20 17:26:48 -0400

    API : Added methods and mapping to edit member info. SPA : Added component to edit member info and save the changes. Added a CanDeactivate guard to member edit component

--- CrmApp-SPA/src/app/app.module.ts
+++ CrmApp-SPA/src/app/app.module.ts
@@ -27,6 +27,9 @@ import { MemberCardComponent } from './members/member-card/member-card.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 
 export function tokenGetter() {
@@ -44,6 +47,7 @@ export function tokenGetter() {
       MessagesComponent,
       MemberCardComponent,
       MemberDetailComponent,
+      MemberEditComponent,
    ],
    imports: [
       BrowserModule,
@@ -69,6 +73,8 @@ export function tokenGetter() {
       UserService,
       MemberDetailResolver,
       MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges,
    ],
    bootstrap: [
       AppComponent
